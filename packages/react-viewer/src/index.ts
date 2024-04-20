9e045adb22b973d72942ab7121b96310aecd70f0
Moriarty47 <wang_jn_xian@163.com>
2024-04-21 02:55:45 +0800

    :bug: export provider components.

--- packages/react-viewer/src/index.ts
+++ packages/react-viewer/src/index.ts
@@ -1,3 +1,4 @@
 import './style.scss';
 export { default as N3bulaViewer } from './viewer';
 export { default as useViewerState } from './hooks/use-viewer-state';
+export { default as N3bulaProvider, ViewerNode } from './registry/registry-provider';
