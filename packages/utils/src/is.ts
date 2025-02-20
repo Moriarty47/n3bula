1f6350fafc2bdd55268bc2ce906ab79898e4a840
Moriarty47 <wang_jn_xian@163.com>
2025-02-21 02:44:12 +0800

    :bug: fix @n3bula/utils 'is' tool type error :art: improve some 'is' method implementation

--- packages/utils/src/is.ts
+++ packages/utils/src/is.ts
@@ -1,54 +1,54 @@
-export type UnionWithException<T extends (string | number)[]> = (Readonly<T>)[number] | (string & {});
+export type ThingType = 
+  | 'string'
+  | 'number'
+  | 'boolean'
+  | 'symbol'
+  | 'bigint'
+  | 'null'
+  | 'undefined'
+  | 'array'
+  | 'object'
+  | 'date'
+  | 'regexp'
+  | 'function'
+  | (string & {});
 
-export type ThingType = UnionWithException<[
-  'string',
-  'number',
-  'boolean',
-  'symbol',
-  'bigInt',
-  'null',
-  'undefined',
-  'array',
-  'object',
-  'date',
-  'regexp',
-  'function'
-]>;
 
 export type PrimaryType = number | string | boolean | symbol | bigint | null | undefined;
 
 export const getType = (thing: unknown): string =>
   Object.prototype.toString.call(thing).slice(8, -1).toLowerCase();
 
-export const isType = (thing: unknown, type: ThingType): boolean =>
+export const isType = <T>(thing: unknown, type: ThingType): thing is T =>
   getType(thing) === type.toLowerCase();
 
-export const isNumber = (thing: unknown): thing is number => isType(thing, 'number');
+export const isNumber = (thing: unknown): thing is number => typeof thing === 'number';
 
-export const isString = (thing: unknown): thing is string => isType(thing, 'string');
+export const isString = (thing: unknown): thing is string => typeof thing === 'string';
 
-export const isBoolean = (thing: unknown): thing is boolean => isType(thing, 'boolean');
+export const isBoolean = (thing: unknown): thing is boolean => typeof thing === 'boolean';
 
-export const isBigInt = (thing: unknown): thing is bigint => isType(thing, 'bigint');
+export const isBigInt = (thing: unknown): thing is bigint => typeof thing === 'bigint';
 
-export const isSymbol = (thing: unknown): thing is symbol => isType(thing, 'symbol');
+export const isSymbol = (thing: unknown): thing is symbol => typeof thing === 'symbol';
 
-export const isNull = (thing: unknown): thing is null => isType(thing, 'null');
+export const isFunction = (thing: unknown): thing is Function => typeof thing === 'function';
 
-export const isUndefined = (thing: unknown): thing is undefined => isType(thing, 'undefined');
+export const isObject = (thing: unknown): thing is object => !isNull(thing) && typeof thing === 'object';
+
+export const isNull = (thing: unknown): thing is null => thing === null;
+
+export const isUndefined = (thing: unknown): thing is undefined => thing === undefined;
 
 export const isNullable = (thing: unknown): thing is null | undefined => isNull(thing) || isUndefined(thing);
 
-export const isPrimary = (thing: unknown): thing is PrimaryType => {
-  if (thing !== null && (typeof thing === 'object' || typeof thing === 'function')) return false;
-  return true;
-};
+export const isPrimary = (thing: unknown): thing is PrimaryType => !(thing !== null && (typeof thing === 'object' || typeof thing === 'function'));
 
-export const isArray = (thing: unknown): thing is any[] => isType(thing, 'array');
+export const isArray = (thing: unknown): thing is any[] => Array.isArray(thing);
 
-export const isFunction = (thing: unknown): thing is () => void => isType(thing, 'function');
+export const isDate = (thing: unknown): thing is Date => isType<Date>(thing, 'date');
 
-export const isObject = (thing: unknown): thing is {} => isType(thing, 'object');
+export const isRegExp = (thing: unknown): thing is RegExp => isType<RegExp>(thing, 'regexp');
 
 export const isBrowser = (): boolean => !!(
   typeof window !== 'undefined' && window?.document?.createElement
